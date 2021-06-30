672a77a0a72d0c23feb740d40012c7a08e5bda89
sivasankar.s <sivasankar.s@listertechnologies.com>
2021-06-30 08:56:11 +0530

    Add the init in onattach method

--- android/src/main/java/com/example/oracle_responsys_sdk/OracleResponsysSdkPlugin.java
+++ android/src/main/java/com/example/oracle_responsys_sdk/OracleResponsysSdkPlugin.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.webkit.WebView;
 
 import androidx.annotation.NonNull;
+
 import java.util.Map;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -17,7 +18,9 @@ import io.flutter.plugin.common.MethodChannel.Result;
 
 import com.oracle.cx.mobilesdk.ORABaseConfigSettings;
 import com.oracle.cx.mobilesdk.ORADataCollector;
+import com.oracle.cx.mobilesdk.ORAEventMedia;
 import com.oracle.cx.mobilesdk.ORAEventMeta;
+import com.oracle.cx.mobilesdk.ORAProductMeta;
 import com.oracle.cx.mobilesdk.contracts.IORAConfigSetting;
 import com.oracle.cx.mobilesdk.exceptions.ORAModuleIDException;
 import com.oracle.cx.mobilesdk.persistent.ORACoreDataContainer;
@@ -33,7 +36,7 @@ public class OracleResponsysSdkPlugin implements FlutterPlugin, MethodCallHandle
     private MethodChannel channel;
     private Activity activity;
     private static String CHANNEL_NAME = "oracle_responsys_sdk";
-    private static String CUSTOM_PARAMS = "customParams";
+//    private static String CUSTOM_PARAMS = "customParams";
     private static String CUSTOM_DATA = "customData";
     private static String EVENT_PATH = "eventPath";
     private static String EVENT_TYPE = "eventType";
@@ -45,6 +48,19 @@ public class OracleResponsysSdkPlugin implements FlutterPlugin, MethodCallHandle
     private static String ERROR_MESSAGE = "errorMessage";
     private static String AD_NAME = "adName";
     private static String AD_NAMES = "adNames";
+    private static String CONTENT_GROUP = "contentGroup";
+    private static String CONVERSION_NAME = "conversionName";
+    private static String MEDIA_NAME = "mediaName";
+    private static String MEDIA_TYPE = "mediaType";
+    private static String MEDIA_EVENT_TYPE = "mediaEventType";
+    private static String TYPE = "type";
+    private static String PRODUCT_SUB_TOTAL = "productSubTotal";
+    private static String PRODUCT_UNIT = "productUnit";
+    private static String PRODUCT_ID = "productId";
+    private static String PRODUCT_SKU = "productSku";
+    private static String INVOICE_NUMBER = "invoiceNumber";
+    private static String SEARCH_PHRASE = "searchPhrase";
+    private static String SEARCH_RESULT = "searchResult";
     private static String ARGUMENTS = "arguments";
     private ORACoreDataContainer container;
 
@@ -54,14 +70,15 @@ public class OracleResponsysSdkPlugin implements FlutterPlugin, MethodCallHandle
         channel.setMethodCallHandler(this);
         init();
     }
-    private void init(){
-            try {
-                ORADataCollector.setApplication(activity.getApplication());
-                container = new ORACoreDataContainer(activity);
-                container.loadFromConfigFile();
-            } catch (ORAModuleIDException e) {
-                e.printStackTrace();
-            }
+
+    private void init() {
+        try {
+            ORADataCollector.setApplication(activity.getApplication());
+            container = new ORACoreDataContainer(activity);
+            container.loadFromConfigFile();
+        } catch (ORAModuleIDException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -111,18 +128,17 @@ public class OracleResponsysSdkPlugin implements FlutterPlugin, MethodCallHandle
     }
 
     public Map<String, String> triggerApplicationStartEvent(Map<String, Object> value) {
-        Map<String, String> childMap = (Map<String, String>) value.get("customKey");
-        return ORADataCollector.getInstance().triggerApplicationStartEvent((String) value.get(APPLICATION_NAME), (Map<String, String>) value.get(CUSTOM_PARAMS));
+        return ORADataCollector.getInstance().triggerApplicationStartEvent((String) value.get(APPLICATION_NAME), (Map<String, String>) value.get(CUSTOM_DATA));
     }
 
 
     public Map<String, String> triggerApplicationErrorEvent(Map<String, Object> value) {
-        return ORADataCollector.getInstance().triggerApplicationErrorEvent((String) value.get(APPLICATION_NAME), (String) value.get(ERROR_MESSAGE), (Map<String, String>) value.get(CUSTOM_PARAMS));
+        return ORADataCollector.getInstance().triggerApplicationErrorEvent((String) value.get(APPLICATION_NAME), (String) value.get(ERROR_MESSAGE), (Map<String, String>) value.get(CUSTOM_DATA));
     }
 
 
     public Map<String, String> triggerApplicationTerminateEvent(Map<String, Object> value) {
-        return ORADataCollector.getInstance().triggerApplicationTerminateEvent((String) value.get(APPLICATION_NAME), (Map<String, String>) value.get(CUSTOM_PARAMS));
+        return ORADataCollector.getInstance().triggerApplicationTerminateEvent((String) value.get(APPLICATION_NAME), (Map<String, String>) value.get(CUSTOM_DATA));
     }
 
 
@@ -180,69 +196,140 @@ public class OracleResponsysSdkPlugin implements FlutterPlugin, MethodCallHandle
     triggerAdClickEvent(Map<String, Object> value) {
         ORAEventMeta eventMeta;
         if (value.containsKey(EVENT_TYPE)) {
-            eventMeta = new ORAEventMeta(EVENT_PATH, EVENT_DESCRIPTION, EVENT_TYPE, (Map<String, String>) value.get(CUSTOM_DATA));
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
         } else {
-            eventMeta = new ORAEventMeta(EVENT_PATH, EVENT_DESCRIPTION, (Map<String, String>) value.get(CUSTOM_DATA));
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
         }
         return ORADataCollector.getInstance().triggerAdClickEvent(eventMeta, (String) value.get(AD_NAME));
     }
 
 
     public Map<String, String> triggerAdImpressionEvent(Map<String, Object> value) {
-        ORAEventMeta eventMeta = new ORAEventMeta(EVENT_PATH, EVENT_DESCRIPTION, EVENT_TYPE, (Map<String, String>) value.get(CUSTOM_DATA));
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
         return ORADataCollector.getInstance().triggerAdImpressionEvent(eventMeta, (String[]) value.get(AD_NAMES));
     }
 
 
     public Map<String, String> triggerConversionEvent(Map<String, Object> value) {
-        return null;
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerConversionEvent(eventMeta, (String) value.get(CONTENT_GROUP), (String) value.get(CONVERSION_NAME));
     }
 
 
     public Map<String, String> triggerMediaEvent(Map<String, Object> value) {
-        return null;
+        ORAEventMedia eventMedia = new ORAEventMedia((String) value.get(MEDIA_NAME), (String) value.get(MEDIA_TYPE), (String) value.get(MEDIA_EVENT_TYPE));
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerMediaEvent(eventMeta, (String) value.get(CONTENT_GROUP), eventMedia);
     }
 
 
     public Map<String, String> triggerProductEvent(Map<String, Object> value) {
-        return null;
+        ORAProductMeta productMeta = new ORAProductMeta((String) value.get(TYPE), (String[]) value.get(PRODUCT_SUB_TOTAL), (String[])
+                value.get(PRODUCT_UNIT), (String[]) value.get(PRODUCT_ID), (String[]) value.get(PRODUCT_SKU));
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerProductEvent(eventMeta, (String) value.get(CONTENT_GROUP), productMeta);
     }
 
 
     public Map<String, String> triggerCartTransactionEvent(Map<String, Object> value) {
-        return null;
+        ORAProductMeta productMeta = new ORAProductMeta((String) value.get(TYPE), (String[]) value.get(PRODUCT_SUB_TOTAL), (String[])
+                value.get(PRODUCT_UNIT), (String[]) value.get(PRODUCT_ID), (String[]) value.get(PRODUCT_SKU));
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerCartTransactionEvent(eventMeta, (String) value.get(CONTENT_GROUP), productMeta, (String) value.get(INVOICE_NUMBER));
     }
 
 
     public Map<String, String> triggerScreenViewEvent(Map<String, Object> value) {
-        return null;
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerScreenViewEvent(eventMeta, (String) value.get(CONTENT_GROUP));
     }
 
 
     public Map<String, String> triggerContentViewEvent(Map<String, Object> value) {
-        return null;
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerContentViewEvent(eventMeta, (String) value.get(CONTENT_GROUP));
     }
 
 
     public Map<String, String> triggerSearchEvent(Map<String, Object> value) {
-        return null;
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerSearchEvent(eventMeta, (String) value.get(SEARCH_PHRASE), (String) value.get(SEARCH_RESULT));
     }
 
 
     public Map<String, String> triggerButtonClickEvent(Map<String, Object> value) {
-        return null;
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerButtonClickEvent(eventMeta);
     }
 
 
     public Map<String, String> triggerCustomEvent(Map<String, Object> value) {
-
-        return null;
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+        return ORADataCollector.getInstance().triggerCustomEvent(eventMeta);
     }
 
 
     public Map<String, String> triggerVideoLoadEvent(Map<String, Object> value) {
+        ORAEventMeta eventMeta;
+        if (value.containsKey(EVENT_TYPE)) {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (String) value.get(EVENT_TYPE), (Map<String, String>) value.get(CUSTOM_DATA));
+        } else {
+            eventMeta = new ORAEventMeta((String) value.get(EVENT_PATH), (String) value.get(EVENT_DESCRIPTION), (Map<String, String>) value.get(CUSTOM_DATA));
+        }
+//        return ORADataCollector.getInstance().triggerVideoLoadEvent(eventMeta);
         return null;
     }
+
 
 
     public Map<String, String> triggerVideoLoadMetaEvent(Map<String, Object> value) {
--- lib/oracle_responsys_sdk.dart
+++ lib/oracle_responsys_sdk.dart
@@ -25,4 +25,123 @@ class OracleResponsysSdk {
         .invokeMethod('triggerApplicationStartEvent', {ARGUMENTS: arguments});
     return version;
   }
+
+  static Future<Map<String, String>> triggerApplicationErrorEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerApplicationErrorEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerApplicationTerminateEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel.invokeMethod(
+        'triggerApplicationTerminateEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerActivityStartEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel.invokeMethod(
+        'triggerApplicationTerminateEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerApplicationEnterBackgroundEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel.invokeMethod(
+        'triggerApplicationEnterBackgroundEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerApplicationEnterForegroundEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel.invokeMethod(
+        'triggerApplicationEnterForegroundEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerActivityResumeEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerActivityResumeEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerActivityPauseEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerActivityPauseEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerActivityStopEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerActivityStopEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerFragmentStartEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerActivityStopEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerFragmentResumeEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerFragmentResumeEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerFragmentPauseEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerFragmentPauseEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerFragmentStopEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerFragmentStopEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerAdClickEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerAdClickEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerAdImpressionEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerAdImpressionEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerConversionEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerConversionEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerMediaEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerMediaEvent', {ARGUMENTS: arguments});
+    return version;
+  }
+
+  static Future<Map<String, String>> triggerProductEvent(
+      Map<String, Object> arguments) async {
+    final Map<String, String> version = await _channel
+        .invokeMethod('triggerProductEvent', {ARGUMENTS: arguments});
+    return version;
+  }
 }
